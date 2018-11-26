0f312d385edc394e7e09fdd8c71580e2c7b6302b
wxke <wangxkwang@outlook.com>
2018-11-26 20:57:24 +0800

    更改用户重名问题

--- app_test/views.py
+++ app_test/views.py
@@ -8,9 +8,11 @@ def index(request):
         pass2 = request.POST.get("Password2",None)
         if pass1 != pass2:
             return render(request, 'index.html',{'ig2':"两次密码不一样"})
-        elif  models.User.objects.filter(UserName = "name").first():
-           return render(request,'index.html',{'ig2':"用户已存在"})
+        elif models.User.objects.filter(UserName= name).first():
+            print(1)
+            return render(request,'index.html',{'ig2':"用户已存在"})
         else:
+            print(2)
             models.User.objects.create(UserName=name,password=pass1)
             return render(request, 'index.html')
     else:
