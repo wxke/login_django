83b7bf2f0a98d436f1efed52d0a79adb9fd8c0b5
wxke <wangxkwang@outlook.com>
2018-11-27 11:36:41 +0800

    注册加了Ajax请求
    Signed-off-by: wxke <wangxkwang@outlook.com>

--- app_test/views.py
+++ app_test/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from app_test import models
+import json
 # Create your views here.
 def index(request):
+    dic = {"status": True, "error": ''}
     if request.method == "POST":
         name = request.POST.get("Name",None)
         pass1 = request.POST.get("Password1",None)
         pass2 = request.POST.get("Password2",None)
-        if pass1 != pass2:
-            return render(request, 'index.html',{'ig2':"两次密码不一样"})
-        elif models.User.objects.filter(UserName= name).first():
-            return render(request,'index.html',{'ig2':"用户已存在"})
+        print(name,pass1,pass2)
+        if models.User.objects.filter(UserName= name).first():
+            # return render(request,'index.html',{'ig2':"用户已存在"})
+            dic['status'] = False
+            dic['error'] = "用户已存在"
+            return HttpResponse(json.dumps(dic))
+        elif pass1 != pass2:
+            dic['status'] = False
+            dic['error'] = "两次密码不一样"
+            # return render(request, 'index.html',{'ig2':"两次密码不一样"})
+            return HttpResponse(json.dumps(dic))
         else:
             models.User.objects.create(UserName=name,password=pass1)
-            return render(request, 'index.html')
+            dic['error'] = "注册成功"
+            return HttpResponse(json.dumps(dic))
+            # return render(request, 'index.html')
     else:
         return render(request,'index.html')
+
 
 
 def home(request):
